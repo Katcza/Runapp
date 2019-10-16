fc33bd12a3c24e5b16ce74ecb40f32e8a475770d
Katcza <48223249+Katcza@users.noreply.github.com>
2019-10-16 15:00:08 +0200

    CloseTheApp

--- runapp/runapp/MainWindow.xaml.cs
+++ runapp/runapp/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace runapp
         {
             InitializeComponent();
         }
+
+        private void ShutdownButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
